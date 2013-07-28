ad845fdbddde7469b40237124d07ce452d428538
Shashir Bharadwaj <shashir87@gmail.com>
2013-07-29 04:46:08 +0530

    Files

---
+++ Serialization/Employee.java
@@ -0,0 +1,10 @@
+public class Employee implements java.io.Serializable
+{
+   public String name;
+   public String address;
+   public int number;
+   public void printHello()
+   {
+      System.out.println("Hello World");
+   }
+}
